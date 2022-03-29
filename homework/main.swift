5588de4276de16db22f52dbb0cc72ab21a473c11
grinevich <grinevichyu@gmail.com>
2022-03-29 18:07:46 +0300

    homework3 is done

--- homework/main.swift
+++ homework/main.swift
@@ -8,4 +8,24 @@
 import Foundation
 
 
+struct SportCar {
+    let brand: String
+    let year: Int
+    let color: String
+    let trunkVolume: Int
+    let isEngineWorking: Bool
+    let isWindowsOpen: Bool
+}
+
+struct TrunkCar {
+    let brand: String
+    let year: Int
+    let color: String
+    let trunkVolume: Int
+    let isEngineWorking: Bool
+    let isWindowsOpen: Bool
+}
+
+var trunkOne = TrunkCar(brand: "Kamaz", year: 2010, color: "Yellow", trunkVolume: 5600, isEngineWorking: true, isWindowsOpen: false)
+
 
