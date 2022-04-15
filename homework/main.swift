5d820e91f49182950dd3396eb245359f628cab30
grinevich <grinevichyu@gmail.com>
2022-04-15 20:38:27 +0300

    added boocking service

--- homework/main.swift
+++ homework/main.swift
@@ -7,21 +7,94 @@
 
 import Foundation
 
+enum BookingServiceErrors: Error {
+    case seatUnaviable
+    case notEnoughMoney
+    case noSuchDestination
+}
+
 struct Flight {
-    let amountOfSeats: Int
+    var amountOfSeats: Int
     let price: Int
-    let flightNumber: Int
     let destination: Destination
 }
 
 struct Destination {
     let name: String
+    let flightNumber: Int
 }
 
 class BookingService {
-    let tickets = [
-        "Moscow": Flight(amountOfSeats: 43, price: 2500, flightNumber: 1375, destination: Destination(name: "Moscow")),
-        "Vladivostok": Flight(amountOfSeats: 350, price: 18000, flightNumber: 1700, destination: Destination(name: "Vladivostok")),
-        "Novosibirsk": Flight(amountOfSeats: 0, price: 5000, flightNumber: 1652, destination: Destination(name: "Novosibirsk"))
+    
+    private var tickets = [
+        "Moscow": Flight(amountOfSeats: 43, price: 2500, destination: Destination(name: "Moscow", flightNumber: 1375)),
+        "Vladivostok": Flight(amountOfSeats: 350, price: 18000, destination: Destination(name: "Vladivostok", flightNumber: 1700)),
+        "Novosibirsk": Flight(amountOfSeats: 1, price: 5000, destination: Destination(name: "Novosibirsk", flightNumber: 1653))
     ]
+    
+    public var money: Int = 0
+    
+    func depositMoney(amount: Int) {
+        money += amount
+    }
+    
+    func buyTicket(to destination: String) throws -> Destination? {
+        
+        guard let flight = tickets[destination] else { throw BookingServiceErrors.noSuchDestination}
+        guard flight.amountOfSeats > 0 else { throw BookingServiceErrors.seatUnaviable}
+        guard money >= flight.price else { throw BookingServiceErrors.notEnoughMoney}
+        
+        var newFlight = flight
+        
+        newFlight.amountOfSeats -= 1
+        tickets[destination] = newFlight
+        money -= flight.price
+        
+        return flight.destination
+    }
 }
+
+let ticket = BookingService()
+print("Money left: \(ticket.money)")
+ticket.depositMoney(amount: 15000)
+print("Money left: \(ticket.money)")
+
+do {
+    let firstTicket = try ticket.buyTicket(to: "Moscow")
+    print("You succesfully bought ticket to destination: \(firstTicket?.name ?? ""), flight number: \(firstTicket?.flightNumber ?? 0)")
+} catch {
+    print(error)
+}
+
+do {
+    let secondTicket = try ticket.buyTicket(to: "Novosibirsk")
+    print("You succesfully bought ticket to destination: \(secondTicket?.name ?? ""), flight number: \(secondTicket?.flightNumber ?? 0)")
+} catch {
+    print(error)
+}
+
+do {
+    let thirdTicket = try ticket.buyTicket(to: "Novosibirsk")
+    print("You succesfully bought ticket to destination: \(thirdTicket?.name ?? ""), flight number: \(thirdTicket?.flightNumber ?? 0)")
+} catch {
+    print(error)
+}
+
+ticket.depositMoney(amount: 12000)
+
+do {
+    let forthTicket = try ticket.buyTicket(to: "Novosibirsk")
+    print("You succesfully bought ticket to destination: \(forthTicket?.name ?? ""), flight number: \(forthTicket?.flightNumber ?? 0)")
+} catch {
+    print(error)
+}
+
+do {
+    let fifthTicket = try ticket.buyTicket(to: "Novosibisrk")
+    print("You succesfully bought ticket to destination: \(fifthTicket?.name ?? ""), flight number: \(fifthTicket?.flightNumber ?? 0)")
+} catch {
+    print(error)
+}
+
+print("Money left: \(ticket.money)")
+
